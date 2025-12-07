aa8a8fb2e166c4e2d7aca4b4a73e970dcafc9bda
NightBo419 <goldenkey99012@gmail.com>
2025-12-07 18:50:35 +0700

    Update

--- dataset.py
+++ dataset.py
@@ -231,7 +231,7 @@ class ZSLDataset(data.Dataset):
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
-    print(path)
+    # print(path)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
